7b929494e9650e9d230e1379f6d19234a1e674cd
primboi <prianshmittra@gmail.com>
2021-11-14 13:52:37 -0800

    Commit

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,7 @@
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const prompt = require('prompt-sync')();
 let toprow = ["_", "_", "_"];
@@ -13,11 +13,32 @@ console.log("");
 console.log("B ", middlerow);
 console.log("");
 console.log("C", bottomrow);
-while(true){
-    let question = prompt("Enter Move > ");
-    if (question == "exit"){
-        console.log("Cleaning Board, and exiting, bye!");
-        await delay(100);
-        process.exit();
+let startprompt = prompt("Player 1 is represented by \"0\" and player 2 is represented by \"X\". Click enter to continue...");
+let name1 = prompt("Player 1, what is your name? > ");
+let name2 = prompt("Player 2, what is your name? > ");
+while (true) {
+    while (true) {
+        let player1 = prompt("Player 1 Enter Move > ");
+        if (player1 == "exit") {
+            console.log("Cleaning Board, and exiting, bye!");
+            process.exit();
+        }
+        if (player1 == "A1") {
+            toprow[0].push("0");
+            break;
+        }
+        if (player1 == "A2") {
+            toprow[1].push("0");
+            break;
+        }
+        if (player1 == "test"){
+            break;
+        }
+    }
+    while (true) {
+        let player2 = prompt("Player 2 Enter Move > ");
+        if (player2 == "test"){
+            break;
+        }
     }
 }
