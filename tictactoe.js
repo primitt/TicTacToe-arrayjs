15bf75d0155634b68fc6adaa53050c9b35c4141c
primboi <prianshmittra@gmail.com>
2021-11-14 10:33:03 -0800

    -Created board and installed packages

--- tictactoe.js
+++ tictactoe.js
@@ -2,5 +2,21 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
-let toprow = [_, _, _]
-console.log(toprow)
+const prompt = require('prompt-sync')();
+let toprow = ["_", "_", "_"]
+let middlerow = ["_", "_", "_"]
+let bottomrow = ["_", "_", "_"]
+console.log("A ", toprow)
+console.log("")
+console.log(middlerow)
+console.log("")
+console.log(bottomrow)
+while(true){
+    let question = prompt("Enter Move > ")
+    if (question == "exit"){
+        console.log("Cleaning Board, and exiting, bye!")
+        setTimeout(function(){
+            break
+        }, 1000)
+    }
+}
