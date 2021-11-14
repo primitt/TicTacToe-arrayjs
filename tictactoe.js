f9a02f24c7a945791dbf4bfb05e665c8b6841372
primboi <prianshmittra@gmail.com>
2021-11-14 10:42:14 -0800

    Changed formatting and added semicolons

--- tictactoe.js
+++ tictactoe.js
@@ -2,21 +2,22 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 const prompt = require('prompt-sync')();
-let toprow = ["_", "_", "_"]
-let middlerow = ["_", "_", "_"]
-let bottomrow = ["_", "_", "_"]
-console.log("A ", toprow)
-console.log("")
-console.log(middlerow)
-console.log("")
-console.log(bottomrow)
+let toprow = ["_", "_", "_"];
+let middlerow = ["_", "_", "_"];
+let bottomrow = ["_", "_", "_"];
+console.log("1   2   3");
+console.log("A ", toprow);
+console.log("");
+console.log("B ", middlerow);
+console.log("");
+console.log("C", bottomrow);
 while(true){
-    let question = prompt("Enter Move > ")
+    let question = prompt("Enter Move > ");
     if (question == "exit"){
-        console.log("Cleaning Board, and exiting, bye!")
-        setTimeout(function(){
-            break
-        }, 1000)
+        console.log("Cleaning Board, and exiting, bye!");
+        await delay(100);
+        process.exit();
     }
 }
